d5360a96c2e136bb267d0971036078eb656579e8
Carl Joseph <carljoseph156@gmail.com>
2025-01-19 20:43:48 -0500

    Added CORS communication between Frontend and Backend

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const connectionWithDB = require("./connection/DB");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -17,6 +18,14 @@ const port = process.env.PORT || 5000;
 app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 // Our Connection to MongoDB
 connectionWithDB();
